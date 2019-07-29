0fa0bb94f2be022f029783178b300a321c4e47d4
LG <LG@DESKTOP-VAU2QAQ>
2019-07-29 10:13:18 -0300

    Commint 10:12 29/07/2019

--- visao/cabecalho.php
+++
@@ -1,9 +0,0 @@
-<div id="tudo" style="background-color: #A97FBD">
-
-<div class="logo">
-  <p style="text-align:center;"><a href="#"><img src="publico/img/logo.png"></a></p>
-</div>
-
-</div>
-
-<br>
--- visao/template.php
+++ visao/template.php
@@ -1,33 +1,207 @@
+
+
+<!doctype html>
 <html class="no-js" lang="en" dir="ltr">
-    <head>
-        <title>Modas LM</title>
-        <base href="<?= URL_BASE ?>"><!--Esta instrução é super importante e não deve ser mudada!-->
-        
-    <link rel="stylesheet" type="text/css" href="./publico/css/estilo.css">
+  <head>
+   <link rel="shortcut icon" href="./publico/imagens/logo2.png"/>
+ <meta charset="utf-8">
+   
+    <title>Modas LM</title>
+    <base href="<?= URL_BASE ?>">
+
+<link rel="stylesheet" type="text/css" href="./publico/css/estilo.css">
     <link rel="stylesheet" href="./publico/css/app.css">
-    </head>
-    
-     <?php include('cabecalho.php');?>
 
-    <body class="container">
-        
-        <div class="corpinho">
-            <div class="caixinha">
+    <link rel="stylesheet" href="./publico/css/foundation.css">
+    <link rel="stylesheet" href="./publico/css/app.css">
+    <link rel="stylesheet" type="text/css" href="./publico/css/estilo.css">
 
+  <!-- foundation-float.min.css: Compressed CSS with legacy Float Grid -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.5.0/dist/css/foundation-float.min.css" integrity="sha256-6gKcI9FMwYa0cREDijDPEanZX+TXP5FQsAYxgP3PGeY= sha384-whJLw6nWgfRYEj1Vmfxc1YCBXOxktFK7lXPFi5htbBtHofjJt5WnWFWrXmSpAACM sha512-q2pM7Q5U4ewl6vP0upeWuhDCSIecXB4zTXpz7pmy8ir4/YtnrW4CuZT7oh4zgo+7Z9N5qxHiApfwN2ckkdJXIg==" crossorigin="anonymous">
+</head>
+  <body class="container">
+   
+<div id="tudo" style="background-color: #A97FBD">
+
+       
+<div class="m">
+  <div class="row">
+  <div class="columns">
+    <ul class="dropdown menu" style="border-color:#A97FBD" data-dropdown-menu>
+      <li>
+        <a href="#Item-2"><i class="fi-list" style="color:black;font-size:20px;padding:1%;"></i></a>
+        <ul class="menu">
+          <li>
+            
+<div class="row">
+  <div class="columns">
+    <ul class="dropdown menu" data-dropdown-menu>
+       <li>
+        <a href="#Item-2">Infantil</a>
+         <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="#Item-2">Calçados</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Tênis</a></li>
+          <li><a href="#Item-2B">Sandálias</a></li>
+          <li><a href="#Item-2A">Sapatos</a></li>
+          <li><a href="#Item-2B">Sapatilhas</a></li>
+          <li><a href="#Item-2A">Chinelos</a></li>
+          <li><a href="#Item-2B">Botas</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#Item-2">Roupas</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Conjuntos</a></li>
+          <li><a href="#Item-2B">Blusas e Camisetas</a></li>
+           <li><a href="#Item-2A">Vestidos</a></li>
+          <li><a href="#Item-2B">Pijamas e Camisolas</a></li>
+           <li><a href="#Item-2A">Fantasias</a></li>
+          <li><a href="#Item-2B">Calças</a></li>
+        </ul>
+      </li>
+       <li>
+        <a href="#Item-2">Acessórios</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Bonés e Chapéus</a></li>
+          <li><a href="#Item-2B">Óculos de sol</a></li>
+           <li><a href="#Item-2A">Cintos</a></li>
+          <li><a href="#Item-2B">Mochilas</a></li>
+        </ul>
+      </li>
+    </ul>
+      </li>
+      <li>
+        <a href="#Item-2">Masculino</a>
+        <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="#Item-2">Calçados</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Chinelos</a></li>
+          <li><a href="#Item-2B">Sapatênis</a></li>
+          <li><a href="#Item-2A">Tênis</a></li>
+          <li><a href="#Item-2B">Sapato Social</a></li>
+          
+        </ul>
+      </li>
+      <li>
+        <a href="#Item-2">Roupas</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Bermudas</a></li>
+          <li><a href="#Item-2B">Camisetas</a></li>
+          <li><a href="#Item-2A">Moletom</a></li>
+          <li><a href="#Item-2B">Polos</a></li>
+            <li><a href="#Item-2B">Calças</a></li>
+            <li><a href="#Item-2B">Camisas</a></li>
+        </ul>
+      </li>
+       <li>
+        <a href="#Item-2">Acessórios</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Óculos de Sol</a></li>
+          <li><a href="#Item-2B">Relógios</a></li>
+           <li><a href="#Item-2A">Bonés</a></li>
+          <li><a href="#Item-2B">Cintos</a></li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+      <li>
+        <a href="#Item-2">Feminino</a>
+        <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="#Item-2">Calçados</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Rasteirinhas</a></li>
+          <li><a href="#Item-2B">Tênis</a></li>
+           <li><a href="#Item-2A">Sapatos</a></li>
+          <li><a href="#Item-2B">Chinelos</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#Item-2">Roupas</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Vestidos</a></li>
+          <li><a href="#Item-2B">Calças</a></li>
+           <li><a href="#Item-2A">Blusas</a></li>
+          <li><a href="#Item-2B">Saias</a></li>
+           <li><a href="#Item-2A">Bodys</a></li>
+          <li><a href="#Item-2B">Macacões e Macaquinhos</a></li>
+        </ul>
+      </li>
+       <li>
+        <a href="#Item-2">Acessórios</a>
+        <ul class="menu">
+          <li><a href="#Item-2A">Bolsas</a></li>
+          <li><a href="#Item-2B">Relógios</a></li>
+           <li><a href="#Item-2A">Bijuterias</a></li>
+          <li><a href="#Item-2B">Lenços</a></li>
+        </ul>
+      </li>
+    </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+            </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+</div>
+<div class="" style="padding:3%;width:28%;">
+  <input id="barra_pesquisa" type="text" placeholder = " Pesquisa..." style="margin:0px;" >
+</div>
+<div class="e">
+  <p style="text-align:center;"><a href="#"><img src="./publico/imagens/logo.png"></a></p>
+</div>
+<div class="e">
+  <a href="login.html"><p id= "icon_Minha_conta" style="color:black;font-size:25px;text-align:center;padding:1%;"><i class="fi-torso" style="margin-right:10px;"></i>Minha Conta</p></a>
+</div>
+<div class="m">
+  <a href="carrinho.html"><p id= "icon_carrinho" style="color: black" ><i style="font-size:40px" class="fi-shopping-cart"></i></p></a>
+</div>
+</div> 
+          </div>
+
+          <div id="subtopo_inicial">
+            
+          </div>
+<br>
+
+<div style="padding: 1rem 1rem;">
         <a href="./categoria/listarCategorias">Categoria</a>
         <a href="./produto/listarProdutos">Produto</a>
         <a href="./cliente/listarClientes">Cliente</a>
+</div>
         
-        <main class="container">
+        
+        <main class="container" style="padding: 1rem 1rem;">
          
             <?php require $viewFilePath; ?>
             
         </main>
 
-        </div>
-        </div>
 
-         <?php include('rodape.php');?>
+          <div id="rodape" style="background-color: #A97FBD">
+          <a href="admin.html"><img style="width: 17px;float: left;margin-left:1% " src="./publico/imagens/admin.png"></a>
+          <p>Av. João Olímpio de Oliveira, 1561 - Vila Asem, Itapetininga - SP, 18202-000</p>
+          <p>Telefone: (15) 3376-9930</p>
+          </div>
 
-    </body>
+
+    <script src="./publico/js/vendor/jquery.js"></script>
+    <script src="./publico/js/vendor/what-input.js"></script>
+    <script src="./publico/js/vendor/foundation.js"></script>
+    <script src="./publico/js/app.js"></script>
+
+  
+</body>  
 </html>
+
